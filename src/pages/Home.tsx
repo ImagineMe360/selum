04abb24be07701ff584d52d157ec719619566f9c
OlaOluwalekan <olalekanbello534@gmail.com>
2025-10-19 11:37:27 +0100

    Initial frontend boilerplate setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+  return (
+    <div className='flex justify-center items-center h-screen bg-gray-100'>
+      <h1 className='text-3xl font-bold text-blue-600' id='home-title'>
+        Welcome to My Fullstack App 🚀
+      </h1>
+    </div>
+  )
+}
